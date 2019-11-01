f033c5b37f1d16fb93aee63b4e4b12bf3160bac4
Ba-Zen <bazenatlas@gmail.com>
2019-11-01 13:07:22 -0400

    prototype model, inheritance

--- sandbox.js
+++ sandbox.js
@@ -1,46 +1,79 @@
-class User {
-  constructor(username, email) {
-    // set up properties
-    this.username = username;
-    this.email = email;
-    this.score = 0;
-  }
-  login() {
-    console.log(`${this.username} just logged in`);
-    return this;
-  }
-  logout() {
-    console.log(`${this.username} just logged out`);
-    return this;
-  }
-  incScore() {
-    this.score++;
-    console.log(`${this.username} has a score of ${this.score}`);
-    return this;
-  }
+function User(username, email) {
+  this.username = username;
+  this.email = email;
 }
 
-class Admin extends User {
-  deleteUser(user) {
-    users = users.filter(u => u.username !== user.username);
-  }
+User.prototype.login = function() {
+  console.log(`${this.username} has logged in`);
+  return this;
+};
+
+User.prototype.logout = function() {
+  console.log(`${this.username} has logged out`);
+  return this;
+};
+
+function Admin(username, email, title) {
+  User.call(this, username, email);
+  this.title = title;
 }
+
+Admin.prototype = Object.create(User.prototype);
 
 const userOne = new User('harry', 'harry@hogwarts.com');
 const userTwo = new User('ronald', 'ronald@hogwarts.com');
-const userThree = new Admin('baz', 'bazen@hogwarts.com');
+const userThree = new Admin('baz', 'bazen@hogwarts.com', 'part time wizard');
 
-let users = [userOne, userTwo, userThree];
-console.log(users);
+console.log(userOne, userTwo, userThree);
+userTwo.login().logout();
 
-userThree.deleteUser(userTwo);
+// class User {
+//   constructor(username, email) {
+//     // set up properties
+//     this.username = username;
+//     this.email = email;
+//     this.score = 0;
+//   }
+//   login() {
+//     console.log(`${this.username} just logged in`);
+//     return this;
+//   }
+//   logout() {
+//     console.log(`${this.username} just logged out`);
+//     return this;
+//   }
+//   incScore() {
+//     this.score++;
+//     console.log(`${this.username} has a score of ${this.score}`);
+//     return this;
+//   }
+// }
 
-console.log(users);
-userOne
-  .login()
-  .incScore()
-  .incScore()
-  .logout();
+// class Admin extends User {
+//   constructor(username, email, title) {
+//     super(username, email);
+//     this.title = title;
+//   }
+//   deleteUser(user) {
+//     users = users.filter(u => u.username !== user.username);
+//   }
+// }
+
+// const userOne = new User('harry', 'harry@hogwarts.com');
+// const userTwo = new User('ronald', 'ronald@hogwarts.com');
+// const userThree = new Admin('baz', 'bazen@hogwarts.com', 'part time wizard');
+
+// let users = [userOne, userTwo, userThree];
+// console.log(users);
+
+// userThree.deleteUser(userTwo);
+
+// console.log(users);
+// userOne
+//   .login()
+//   .incScore()
+//   .incScore()
+//   .logout();
 
 // const userOne = {
 //   username: 'katniss',
